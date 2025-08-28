503848e864f44f84fd8ae54a006f34f6be3ffc71
ivan <ivanf20058@gmail.com>
2025-08-28 22:54:20 +0200

    HomeCController class added

---
+++ src/main/java/com/ivan/pet/HomeController.java
@@ -0,0 +1,12 @@
+package com.ivan.pet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
